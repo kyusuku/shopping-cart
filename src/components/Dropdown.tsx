309434fc97f8b0f8bbeedb321df4f2a5110f92a2
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-04-24 13:20:15 +0700

    Remove cursor-pointer property from dropdown menu

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -9,7 +9,7 @@ export default function Dropdown({ onCategoryChange }: propTypes) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <MenuButton className="inline-flex w-full cursor-pointer justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50">
+        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50">
           Categories
           <ChevronDownIcon
             aria-hidden="true"
