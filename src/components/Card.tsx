5ac603e97aa283f61595801d91259a54d08784f7
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-04-24 13:13:12 +0700

    Add search product bar and filter by category

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,13 +17,17 @@ interface Product {
   };
 }
 
-const Card = () => {
+interface propTypes {
+  searchQuery: string;
+  selectedCategory: string;
+}
+
+const Card = ({ searchQuery, selectedCategory }: propTypes) => {
   const [productArr, setProductArr] = useState<Product[]>([]);
   const [inputCounts, setInputCounts] = useState<{
     [key: number]: number | string;
   }>({});
   const { setItemCount } = useItemCount();
-
   const stars = Array(5).fill(0);
 
   const handleAddToCart = (id: number) => {
@@ -92,73 +96,81 @@ const Card = () => {
 
   return (
     <div className="grid max-w-[75vw] grid-cols-4 gap-4">
-      {productArr.map((product) => (
-        <div
-          key={product.id}
-          className="flex flex-col items-center justify-between gap-4 rounded-xl border-1 border-gray-300 bg-white p-6 shadow-xl"
-        >
-          <img
-            src={product.image}
-            alt={product.title}
-            className="h-48 w-full object-contain"
-          />
-          <h2 className="text-lg font-semibold">{product.title}</h2>
-          <div className="flex w-full flex-col items-center justify-center gap-6">
-            <div className="flex w-full items-center justify-between">
-              <div className="text-2xl font-bold">${product.price}</div>
+      {productArr
+        .filter(
+          (product) =>
+            product.title.toLowerCase().includes(searchQuery.toLowerCase()) &&
+            (selectedCategory === "" ||
+              product.category.toLowerCase() ===
+                selectedCategory.toLowerCase()),
+        )
+        .map((product) => (
+          <div
+            key={product.id}
+            className="flex flex-col items-center justify-between gap-4 rounded-xl border-1 border-gray-300 bg-white p-6 shadow-xl"
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className="h-48 w-full object-contain"
+            />
+            <h2 className="text-lg font-semibold">{product.title}</h2>
+            <div className="flex w-full flex-col items-center justify-center gap-6">
+              <div className="flex w-full items-center justify-between">
+                <div className="text-2xl font-bold">${product.price}</div>
+                <div className="flex items-center justify-center gap-2">
+                  <div className="flex">
+                    {stars.map((_, index) => {
+                      return (
+                        <FaStar
+                          key={index}
+                          size={24}
+                          color={
+                            product.rating.rate > index ? "#F2C265" : "a9a9a9"
+                          }
+                        ></FaStar>
+                      );
+                    })}
+                  </div>
+                  <div className="text-lg font-semibold">
+                    {product.rating.rate}
+                  </div>
+                </div>
+              </div>
               <div className="flex items-center justify-center gap-2">
-                <div className="flex">
-                  {stars.map((_, index) => {
-                    return (
-                      <FaStar
-                        key={index}
-                        size={24}
-                        color={
-                          product.rating.rate > index ? "#F2C265" : "a9a9a9"
-                        }
-                      ></FaStar>
-                    );
-                  })}
+                <div
+                  onClick={() => handleClickDecrease(product.id)}
+                  className="flex size-8 cursor-pointer items-center justify-center rounded-lg bg-[#004687] px-4 py-2 font-bold text-white transition select-none active:scale-90"
+                >
+                  -
                 </div>
-                <div className="text-lg font-semibold">
-                  {product.rating.rate}
+                <input
+                  onChange={(e) => handleInputChange(e, product.id)}
+                  onBlur={() => handleInputBlur(product.id)}
+                  type="number"
+                  className="size-8 appearance-none rounded-md border-2 border-[#004687] text-center font-semibold text-black outline-0"
+                  value={inputCounts[product.id] ?? 1}
+                  max="99"
+                  min="1"
+                />
+                <div
+                  onClick={() => handleClickIncrease(product.id)}
+                  className="flex size-8 cursor-pointer items-center justify-center rounded-lg bg-[#004687] px-4 py-2 font-bold text-white transition select-none active:scale-90"
+                >
+                  +
                 </div>
               </div>
-            </div>
-            <div className="flex items-center justify-center gap-2">
-              <div
-                onClick={() => handleClickDecrease(product.id)}
-                className="flex size-8 cursor-pointer items-center justify-center rounded-lg bg-[#004687] px-4 py-2 font-bold text-white transition select-none active:scale-90"
+              <button
+                onClick={() => handleAddToCart(product.id)}
+                type="button"
+                className="flex w-full cursor-pointer items-center justify-center gap-2 rounded-lg bg-[#004687] px-4 py-2 font-semibold text-white transition active:scale-95"
               >
-                -
-              </div>
-              <input
-                onChange={(e) => handleInputChange(e, product.id)}
-                onBlur={() => handleInputBlur(product.id)}
-                type="number"
-                className="size-8 appearance-none rounded-md border-2 border-[#004687] text-center font-semibold text-black outline-0"
-                value={inputCounts[product.id] ?? 1}
-                max="99"
-                min="1"
-              />
-              <div
-                onClick={() => handleClickIncrease(product.id)}
-                className="flex size-8 cursor-pointer items-center justify-center rounded-lg bg-[#004687] px-4 py-2 font-bold text-white transition select-none active:scale-90"
-              >
-                +
-              </div>
+                <FontAwesomeIcon icon={faCartShopping} />
+                <div>Add To Cart</div>
+              </button>
             </div>
-            <button
-              onClick={() => handleAddToCart(product.id)}
-              type="button"
-              className="flex w-full cursor-pointer items-center justify-center gap-2 rounded-lg bg-[#004687] px-4 py-2 font-semibold text-white transition active:scale-95"
-            >
-              <FontAwesomeIcon icon={faCartShopping} />
-              <div>Add To Cart</div>
-            </button>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
